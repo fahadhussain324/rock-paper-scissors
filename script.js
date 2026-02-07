88cf294f1de5840859a403cfbc58a026ec9eed33
Fahad Hussain <fahadhussain324@outlook.com>
2026-02-07 12:52:25 +0000

    completed RPS game. Note, no user validation. Round counter, score keeper. Tie breaker.

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 'use strict';
+
+// create a score counter for each
+let humanScore = 0;
+let computerScore = 0;
+let r = 0;
+
 // get computer choice using math.random
 function getComputerChoice() {
   let choice = Math.random()
@@ -13,51 +19,63 @@ function getComputerChoice() {
 }
 
 // get human choice, assuming they've typed one of the options correctly
-function getHumanChoice() {
-  let humanChoice = prompt("Please pick rock, paper or scissors: ")
-  return humanChoice
+function getHumanChoice(roundNumber) {
+  let humanChoice = prompt(`Round: ${roundNumber}. Your score: ${humanScore}, Computer's score: ${computerScore}. Please pick rock, paper or scissors: `)
+  return humanChoice;
 }
 
 //use the functions above to assign the value to their choice variables
-const humanChoice = getHumanChoice().toLowerCase();
-const computerChoice = getComputerChoice().toLowerCase();
 
-// create a score counter for each
-let humanScore = 0;
-let computerScore = 0;
 
 function playRound(humanChoice, computerChoice) {
   const winner = `You Win! ${humanChoice} beats ${computerChoice}!`
   const loser = `You Lose! ${computerChoice} beats ${humanChoice}`
-  if (humanChoice === "rock" && computerChoice === "scissors") {
+  if (humanChoice === computerChoice) {
+    console.log("You both played the same. Try again!");
+    return "tie";
+  } else if (humanChoice === "rock" && computerChoice === "scissors") {
     console.log(winner);
     humanScore++;
     console.log(`Your score: ${humanScore}, Computer's score: ${computerScore}`);
-  }
-  else if (humanChoice === "scissors" && computerChoice === "paper") {
+  } else if (humanChoice === "scissors" && computerChoice === "paper") {
     console.log(winner);
     humanScore++;
     console.log(`Your score: ${humanScore}, Computer's score: ${computerScore}`);
-  }
-  else if (humanChoice === "paper" && computerChoice === "rock") {
+  } else if (humanChoice === "paper" && computerChoice === "rock") {
     console.log(winner);
     humanScore++;
     console.log(`Your score: ${humanScore}, Computer's score: ${computerScore}`);
-  }
-  else {
+  } else {
     console.log(loser);
     computerScore++;
     console.log(`Your score: ${humanScore}, Computer's score: ${computerScore}`);
-
   }
-
+  return "played";
 }
 
 function playGame() {
-  let humanScore = 0;
-  let computerScore = 0;
-  for (r = 0; r < 5; r++) {
-    playRound(humanChoice, computerChoice)
-    console.log(r)
+  let roundsPlayed = 0;
+  let roundNumber = 1;
+
+  while (roundsPlayed < 5) {
+    const humanChoice = getHumanChoice(roundNumber).toLowerCase();
+    const computerChoice = getComputerChoice();
+    const result = playRound(humanChoice, computerChoice);
+  if
+  (result === "played")
+  roundsPlayed++;
+  roundNumber++;
   }
 }
+playGame()
+if (humanScore > computerScore) {
+    console.log("Congrats! You Win the game!")
+    console.log(`Final scores - Your score: ${humanScore}, Computer's score: ${computerScore}`) }
+    else if (computerScore > humanScore) {
+      console.log("Sorry! You Lose the game!")
+      console.log(`Final scores - Your score: ${humanScore}, Computer's score: ${computerScore}`) }
+    else if (humanScore === computerScore) {
+      console.log("You drew the game!")
+      console.log(`Final scores - Your score: ${humanScore}, Computer's score: ${computerScore}`)
+      }
+    else {"Unable to determine winner!"}
